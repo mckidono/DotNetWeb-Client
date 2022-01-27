15f26abc657d6b0ea5196fb2cb5b40dbdc787b21
Donovan McKinney <mckidonno@outlook.com>
2022-01-27 12:43:02 -0600

    Final commit

--- JS/client.js
+++ JS/client.js
@@ -1,7 +1,6 @@
-﻿﻿$(function(){
+﻿﻿﻿$(function(){
     // preload audio
     var toast = new Audio('media/toast.wav');
-
     $('.code').on('click', function(e) {
         e.preventDefault();
         // first pause the audio (in case it is still playing)
@@ -10,10 +9,14 @@
         toast.currentTime = 0;
         // play audio
         toast.play();
+        // display product data
+        $('#product').html($(this).data('product'));
+        $('#code').html($(this).data('code'));
         $('#toast').toast({ autohide: false }).toast('show');
     });
-    $(document).on('keydown', (e) => {
-        if (e.key == 'Escape') {
+
+    $(document).on('keyup', function(e) {
+        if (e.key === "Escape") {
             $('#toast').toast('hide');
         }
     });
